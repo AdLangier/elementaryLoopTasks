53637d39da9f6809d9c464123a011edfd29af9de
Adrian Langier <langier.adrian@gmail.com>
2021-03-11 14:20:08 +0100

    solved task 13

--- src/com/company/Zad13.java
+++ src/com/company/Zad13.java
@@ -5,6 +5,12 @@ import java.util.Scanner;
 public class Zad13 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj ilość gwiazdek");
+        int n = scanner.nextInt();
+
+        for (int i = 1; i <n ; i++) {
+            System.out.print("*");
+        }
 
 
     }
