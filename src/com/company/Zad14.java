9860452e14a615eeb1ea53919257ef8b43bf8eee
Adrian Langier <langier.adrian@gmail.com>
2021-03-12 11:19:06 +0100

    added recurrence for Task14

--- src/com/company/Zad14.java
+++ src/com/company/Zad14.java
@@ -6,20 +6,25 @@ import java.util.Scanner;
 public class Zad14 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Podaj liczbę");                               //Trudne zadanie - połączenie pętli i instrukcji warunkowej
-        int n = scanner.nextInt();                                        //                 + zmienna pomocnicza
         int numberOfDivisors = 0;
 
-        for (int i = 1; i <=n ; i++) {
-            if (n % i == 0) {                          // <---- instrukcja warunkowa "if" bez "else"  !!!
-                numberOfDivisors++;
+        do {
+            System.out.println("Podaj liczbę");                               //Trudne zadanie - połączenie pętli i instrukcji warunkowej
+            int n = scanner.nextInt();
+         //   int numberOfDivisors = 0;                                         //                 + zmienna pomocnicza
+
+
+            for (int i = 1; i <= n; i++) {
+                if (n % i == 0) {                          // <---- instrukcja warunkowa "if" bez "else"  !!!
+                    numberOfDivisors++;
+                }
             }
-        }
-        if(numberOfDivisors <= 2 && numberOfDivisors != 0){               // <--- warunek to "liczbaDzielników może wynosić maksymalnie 2
-            System.out.println("Podana liczba jest liczbą pierwszą");     //     i nie może być zerem (określone również w pętli)
-        }
-        else{
-            System.out.println("Podana liczba jest liczbą złożoną");
-        }
+            if (numberOfDivisors <= 2) {               // <--- warunek to "liczbaDzielników może wynosić maksymalnie 2
+                System.out.println("Podana liczba jest liczbą pierwszą");     //     i nie może być zerem (określone również w pętli)
+            } else {
+                System.out.println("Podana liczba jest liczbą złożoną");
+            }
+        }while (numberOfDivisors !=0);
     }
-}                                //DODADKOWO - WRZUCIĆ POWTARZALNOŚĆ ZAPYTANIA O LICZBĘ (do - while)
+
+}                                     //DODADKOWO - WRZUCIĆ POWTARZALNOŚĆ ZAPYTANIA O LICZBĘ (do - while)
