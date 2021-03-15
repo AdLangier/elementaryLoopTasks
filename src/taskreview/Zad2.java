9a76c1c6122e699074d056571c16aadabd6b1327
Adrian Langier <langier.adrian@gmail.com>
2021-03-15 13:58:27 +0100

    additional Task2

---
+++ src/taskreview/Zad2.java
@@ -0,0 +1,12 @@
+package taskreview;
+
+public class Zad2 {
+    public static void main(String[] args) {
+
+        String inscription = "Hello World";
+
+        for (int i = 0; i <10 ; i++) {
+            System.out.println(inscription);
+        }
+    }
+}
