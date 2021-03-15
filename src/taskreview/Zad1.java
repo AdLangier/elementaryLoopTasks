1fbbd26776acea70a7fba5352f60257e536e796d
Adrian Langier <langier.adrian@gmail.com>
2021-03-15 13:53:40 +0100

    additional Task1

---
+++ src/taskreview/Zad1.java
@@ -0,0 +1,14 @@
+package taskreview;
+
+public class Zad1 {
+    public static void main(String[] args) {
+
+        for (int i = 10; i <=50; i++) {
+            if(i%2!=0) {
+                continue;
+            }
+            System.out.print(i+" ");
+        }
+    }
+
+}
