d2557f40d27cc45d4ed784315b72dfe2385d3a7f
Adrian Langier <langier.adrian@gmail.com>
2021-03-15 14:01:17 +0100

    additional Task3

---
+++ src/taskreview/Zad3.java
@@ -0,0 +1,10 @@
+package taskreview;
+
+public class Zad3 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 19; i++) {
+            System.out.print("* ");
+        }
+    }
+}
